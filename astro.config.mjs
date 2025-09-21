3345bd72ae0e1e226002930c12928ff5fbbb1156
plsedv1 <leser@pls-edv.de>
2025-09-21 10:21:54 +0200

    zweiter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   site: "https://www.pls-edv.de",
-  output: "hybrid", // statisch + SSR gemischt (nur Seiten mit prerender=false sind SSR)
+  output: "server", // statisch + SSR gemischt (nur Seiten mit prerender=false sind SSR)
   adapter: netlify(), // Netlify-Adapter aktivieren
   integrations: [tailwind(), sitemap()],
 });
